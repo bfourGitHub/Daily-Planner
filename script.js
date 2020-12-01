ab695428fd453c2b87c526e54cb37107a9675327
Bryan Moreno <bfourbryan@gmail.com>
2020-11-30 22:05:23 -0800

    Created new function to retrieve saved items in local storage, I figured out that I had to CALL the functionat the end to get the required info to display in the given timeslot, used w3schools for localstorage.getItem function, console.log was used to check if code was working, js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Get the current date
 var today = moment();
-    console.log(today);
+    //console.log(today);
 // Display the current date in the JumboTron
 var currentDay = $("#currentDay").text(today.format("[Today is] dddd, MMMM Do"));
-    console.log(currentDay);
+    //console.log(currentDay);
 
 // Get the current hour of the day so we can later define if it is in the past, present or future
 var currentHour = parseInt(today.format("k"));
-    console.log(currentHour);
+    //console.log(currentHour);
 
 
 // Determining if a timeblock is in the past present or future and assigning a class
 var ppfTime = $(".ppfTime");
-    console.log(ppfTime);
+   // console.log(ppfTime);
 var timeSlotHour = $(".hour");
-    console.log(timeSlotHour);
+    //console.log(timeSlotHour);
 
 //For loop to go through each hour of the day
 for (var i = 0; i < 9; i++) {
@@ -37,19 +37,28 @@ for (var i = 0; i < 9; i++) {
 // Saving input data to local storage with click event listener
 var savedEventBtn = $(".saveBtn");
 savedEventBtn.on("click", function (event) {
-    event.preventDefault();
+    //event.preventDefault();
     var clickedButton = $(this);
     var hourButtonClicked = $(clickedButton).siblings("div").data("time");
     var eventInput = $(clickedButton).prev("textarea").val();
     localStorage.setItem("scheduled-hour-" + hourButtonClicked, eventInput);
     console.log(hourButtonClicked);
+    console.log(clickedButton);
+    console.log("Taylorswift");
 });
 //get the saved text back after the page is reloaded
-function getSavedEvent() {
-    for (var i = 9; i < 18; i++) {
-        eventSaved = $("#saved-event-" + i);
-        var savedEventText = localStorage.getItem("scheduled-hour-" + i, "eventInput");
+// function getSavedEvent() {
+//     for (var i = 9; i < 18; i++) {
+//         eventSaved = $("#saved-event-" + i);
+//         var savedEventText = localStorage.getItem("scheduled-hour-" + i, "eventInput");
         
-        eventSaved.text(savedEventText);
-    }
-}
+//         eventSaved.text(savedEventText);
+//     }
+// }
+
+function myFunction() {
+    var x = localStorage.getItem("scheduled-hour-11");
+    document.getElementById("saved-event-11").innerHTML = x;
+    console.log(x);
+  };
+  myFunction();
