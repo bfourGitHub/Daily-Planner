4b9fb2f8536053843ba2ad90b89e378858602571
Bryan Moreno <bfourbryan@gmail.com>
2020-11-30 16:33:40 -0800

    Addition of html blocks for every hour of work day with appropriate ids to identify hour of day of a given inputs, html

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-console.log("Tom Daley");
-
+// Get the current date
 var today = moment();
 console.log(today);
+// Display the current date in the JumboTron
 var currentDay = $("#currentDay").text(today.format("[Today is] dddd, MMMM Do"));
+console.log(currentDay);
+
+// Get the current hour of the day so we can later define if it is in the past, present or future
+var currentHour = parseInt(today.format("k"));
+console.log(currentHour);
+
+
+// Determining if a timeblock is in the past present or future and assigning a class
+
+
